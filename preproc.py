5b570771e9535c52087f8f0cc94d8bc2fdeee352
farukarakaya <farukarakaya@gmail.com>
2019-01-05 22:30:11 +0300

    feature extraction implementation

--- preproc.py
+++ preproc.py
@@ -2,14 +2,20 @@ import numpy as np
 import skimage.io as imio
 from skimage import color
 import os, glob
-
+import resnet
+import torch
 
 path = os.getcwd()
 train_data_path = path + "/data/train"
+labels = ['n01615121', 'n02099601', 'n02123159',
+          'n02129604', 'n02317335', 'n02391049',
+          'n02410509', 'n02422699', 'n02481823', 'n02504458']
 
 
 def load_all_images():
-    images = glob.glob(train_data_path + '/**/*.JPEG', recursive=True)
+    images = {}
+    for label in labels:
+        images[label] = glob.glob(train_data_path + '/**/*.JPEG', recursive=True)
     return images
 
 
@@ -29,7 +35,31 @@ def normalize(image_file):
         diff = height - width
         padded = np.pad(padded, ((0,0),(diff // 2, diff - diff // 2),(0,0)), mode='constant')
     padded.resize(224,224,3)
+    return padded
 
 
-all_images = load_all_images()
-normalize(all_images[0])
+def extract_feature(image):
+    # we append an augmented dimension to indicate batch_size, which is one
+    image = np.reshape(image, [1, 224, 224, 3])
+    # model takes as input images of size [batch_size, 3, im_height, im_width]
+    image = np.transpose(image, [0, 3, 1, 2])
+    # convert the Numpy image to torch.FloatTensor
+    image = torch.from_numpy(image)
+    image = image.type(torch.FloatTensor)
+    # extract features
+    model = resnet.resnet50(pretrained=True)
+    feature_vector = model.forward(image)
+    # convert the features of type torch.FloatTensor to a Numpy array
+    # so that you can either work with them within the sklearn environment
+    # or save them as .mat files
+    feature_vector = feature_vector.detach().numpy()
+    return feature_vector
+
+def get_all_features_by_labels():
+    all_images = load_all_images()
+    padded_img_vectors_by_label = {}
+    for i,label in labels:
+        padded_img_vectors_by_label[label] = []
+        for img_path in all_images[label]:
+            padded_img_vectors_by_label.append(extract_feature(normalize(img_path)))
+    return padded_img_vectors_by_label
